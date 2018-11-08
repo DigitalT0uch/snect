b127a80846154357cd9d0a5b8d3a216e92e6e20f
Aytac <aytac_07@msn.om>
2018-11-08 14:48:07 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {createClient} from 'contentful'
 
 Vue.config.productionTip = false;
 
@@ -8,3 +9,12 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+window.contentfulClient = createClient({
+  accessToken: '79aa451a69a502a0d00c7fb61264b273fcdc67c349bfb13613aa00306573c521',
+  space: 'k3mp96odf6se'
+});
+
+contentfulClient.getEntries()
+.then((response) => console.log(response.items))
+.catch(console.error);
