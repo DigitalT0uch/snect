5907a64545470365429e774a855bda97384510b9
Aytac <aytac_07@msn.om>
2018-11-09 13:40:18 +0100

    dark theme test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,8 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import darkstyle from "@/styles.js";
+
 
 export default {
   name: "home",
@@ -19,9 +21,40 @@ export default {
     const element = document.getElementById("map")
     const options = {
         zoom: 14,
-        center: new google.maps.LatLng(47.071467, 8.277621)
+        center: new google.maps.LatLng(47.071467, 8.277621),
+        styles: darkstyle
     }
     this.map = new google.maps.Map(element, options);
+
+
+    /*************************** Start Markers ***************************/
+
+
+    let marker = new google.maps.Marker({
+        position: {lat: 47.055193, lng: 8.317648},
+        icon: 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/16/Actions-rating-icon.png',
+        map: this.map
+    });
+
+    marker.setMap(this.map);
+
+
+    const contentString = `<div>
+      <h1>Casineum, Grand Casino Luzern</h1>
+      <h2>Partey</h2>
+      <div>Gits eine?</div> 
+    </div>`;
+
+    let infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    marker.addListener('click', event => {
+      infowindow.open(this.map, marker);
+    }); 
+
+    /***************************  END Markers  ***************************/
+
   }
 };
 </script>
@@ -30,5 +63,6 @@ export default {
 #map {
   height: 80vh;
   width: 80vw;
+  display:inline-block;
 }
 </style>
