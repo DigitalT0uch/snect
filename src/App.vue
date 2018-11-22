cd2ed28c51e676a2f7301d5d1fb9cd4a06e11cb2
Aytac <aytac_07@msn.om>
2018-11-22 18:08:29 +0100

    dont know what i did

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,16 @@
   </div>
 </template>
 
+
+<script>
+function logout(){
+        cookies.deleteCookie("email");
+        cookies.deleteCookie("name");
+        cookies.deleteCookie("lastname");
+        this.$router.push("/");
+      };
+</script>
+
 <!-- Globaler import -->
 <style src="@/assets/css/foundation.css"></style>
 <style src="@/assets/css/style.css"></style>
@@ -58,32 +68,32 @@ body{
 }
 
 .submitbtn{
-        width:70%;
-        flex: 1 1 auto;
-        padding: 20px;
-        text-align: center;
-        text-transform: uppercase;
-          font-size: 4.75vw;
+  width:70%;
+  flex: 1 1 auto;
+  padding: 20px;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 4.75vw;
   font-weight: 700;
   letter-spacing: 0.6vw; 
-        transition: 0.5s;
-        background-size: 200% auto;
-        color: white;
-        cursor:pointer;
-      /* text-shadow: 0px 0px 10px rgba(0,0,0,0.2);*/
-        border:none;
-        border-radius: 30px;
-        /*background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%);*/
-        background-image: linear-gradient(to right, #e6475f 0%, #ef8138 51%, #e6475f 100%);
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  cursor:pointer;
+/* text-shadow: 0px 0px 10px rgba(0,0,0,0.2);*/
+  border:none;
+  border-radius: 30px;
+  /*background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%);*/
+  background-image: linear-gradient(to right, #e6475f 0%, #ef8138 51%, #e6475f 100%);
 
-        &:hover{
-          background-position: right center; /* change the direction of the change here */
-        }
-    }
+  &:hover{
+    background-position: right center; /* change the direction of the change here */
+  }
+}
 
-          .warumnidfullbra{
-      width: 100%;
-      }
+.warumnidfullbra{
+  width: 100%;
+}
 </style>
 
 
